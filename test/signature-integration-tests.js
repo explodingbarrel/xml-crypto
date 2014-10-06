ad23a96045f1dc2f216970d532a31c30cab69e38
mo ko <email>
2014-10-06 14:58:52 -0700

    fixed formatting to ease future editing

--- test/signature-integration-tests.js
+++ test/signature-integration-tests.js
@@ -1,81 +1,81 @@
 var select = require('xpath.js')
-  , Dom = require('xmldom').DOMParser
-  , SignedXml = require('../lib/signed-xml.js').SignedXml
-  , fs = require('fs')
-  , crypto = require('../index')
-  
-module.exports = {    
+	, Dom = require('xmldom').DOMParser
+	, SignedXml = require('../lib/signed-xml.js').SignedXml
+	, fs = require('fs')
+	, crypto = require('../index')
 
-  "verify signature": function (test) {
-  	var xml = "<root><x xmlns=\"ns\"></x><y z_attr=\"value\" a_attr1=\"foo\"></y><z><ns:w ns:attr=\"value\" xmlns:ns=\"myns\"></ns:w></z></root>"
-    verifySignature(test, xml, [
-      "//*[local-name(.)='x']", 
-      "//*[local-name(.)='y']", 
-      "//*[local-name(.)='w']"])
-  },
+module.exports = {
 
-  "verify signature of complex element": function (test) {
-    var xml = "<library>" +
-                "<book>" +
-                  "<name>Harry Potter</name>" +
-                  "<author id=\"123456789\">" +
-                    "<firstName>Joanne K</firstName>" +
-                    "<lastName>Rowling</lastName>" +
-                  "</author>" +
-                "</book>" +
-              "</library>"
+	"verify signature": function (test) {
+		var xml = "<root><x xmlns=\"ns\"></x><y z_attr=\"value\" a_attr1=\"foo\"></y><z><ns:w ns:attr=\"value\" xmlns:ns=\"myns\"></ns:w></z></root>"
+		verifySignature(test, xml, [
+			"//*[local-name(.)='x']",
+			"//*[local-name(.)='y']",
+			"//*[local-name(.)='w']"])
+	},
 
-    verifySignature(test, xml, ["//*[local-name(.)='book']"])
-  },
+	"verify signature of complex element": function (test) {
+		var xml = "<library>" +
+			"<book>" +
+			"<name>Harry Potter</name>" +
+			"<author id=\"123456789\">" +
+			"<firstName>Joanne K</firstName>" +
+			"<lastName>Rowling</lastName>" +
+			"</author>" +
+			"</book>" +
+			"</library>"
 
-  "windows store signature": function(test) {    
+		verifySignature(test, xml, ["//*[local-name(.)='book']"])
+	},
 
-    var xml = fs.readFileSync('./test/static/windows_store_signature.xml', 'utf-8');        
-    var doc = new Dom({ignoreWhiteSpace: true}).parseFromString(xml);    
-    //ensure xml has not white space    
-    xml = doc.firstChild.toString()
+	"windows store signature": function(test) {
 
-    var signature = crypto.xpath(doc, "//*//*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
-    var sig = new crypto.SignedXml();    
-    sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/windows_store_certificate.pem");
-    sig.loadSignature(signature.toString());    
-    var result = sig.checkSignature(xml);
-    test.equal(result, true);
-    test.done();
-  }
+		var xml = fs.readFileSync('./test/static/windows_store_signature.xml', 'utf-8');
+		var doc = new Dom({ignoreWhiteSpace: true}).parseFromString(xml);
+		//ensure xml has not white space
+		xml = doc.firstChild.toString()
+
+		var signature = crypto.xpath(doc, "//*//*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
+		var sig = new crypto.SignedXml();
+		sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/windows_store_certificate.pem");
+		sig.loadSignature(signature.toString());
+		var result = sig.checkSignature(xml);
+		test.equal(result, true);
+		test.done();
+	}
 
 }
 
-function verifySignature(test, xml, xpath) {  
-  if (process.platform !== 'win32') {
-    test.done();
-    return;
-  }
-  var sig = new SignedXml()
-  sig.signingKey = fs.readFileSync("./test/static/client.pem")
-  sig.keyInfoCaluse = null
-  
-  xpath.map(function(n) { sig.addReference(n) })
+function verifySignature(test, xml, xpath) {
+	if (process.platform !== 'win32') {
+		test.done();
+		return;
+	}
+	var sig = new SignedXml()
+	sig.signingKey = fs.readFileSync("./test/static/client.pem")
+	sig.keyInfoCaluse = null
 
-  sig.computeSignature(xml)
-  var signed = sig.getSignedXml()
+	xpath.map(function(n) { sig.addReference(n) })
 
-  fs.writeFileSync("./test/validators/XmlCryptoUtilities/XmlCryptoUtilities/bin/Debug/signedExample.xml", signed)    
-  
-  var spawn = require('child_process').spawn
-  var proc = spawn('./test/validators/XmlCryptoUtilities/XmlCryptoUtilities/bin/Debug/XmlCryptoUtilities.exe', ['verify'])
+	sig.computeSignature(xml)
+	var signed = sig.getSignedXml()
 
-  proc.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-  });
+	fs.writeFileSync("./test/validators/XmlCryptoUtilities/XmlCryptoUtilities/bin/Debug/signedExample.xml", signed)
 
-  proc.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
+	var spawn = require('child_process').spawn
+	var proc = spawn('./test/validators/XmlCryptoUtilities/XmlCryptoUtilities/bin/Debug/XmlCryptoUtilities.exe', ['verify'])
 
-  proc.on('exit', function (code) {   
-    test.equal(0, code, "signature validation failed")
-    test.done()
-  }); 
+	proc.stdout.on('data', function (data) {
+		console.log('stdout: ' + data);
+	});
+
+	proc.stderr.on('data', function (data) {
+		console.log('stderr: ' + data);
+	});
+
+	proc.on('exit', function (code) {
+		test.equal(0, code, "signature validation failed")
+		test.done()
+	});
 
 }
